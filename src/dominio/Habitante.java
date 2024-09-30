d5d5f27a19adb0b73267b21d13e6b8613b4f469d
juanespinoo <juanarturo.espinosarodriguez@usp.ceu.es>
2024-09-30 12:13:22 +0200

    clases de dominio v1

---
+++ src/dominio/Habitante.java
@@ -0,0 +1,22 @@
+package dominio;
+public class Habitante{
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	public Habitante(){}
+	public Habitante (String nombre_, String apellido1_, String apellido2_){
+		nombre=nombre_;
+		apellido1=apellido1_;
+		apellido2=apellido2_;
+	}
+	public String getNombre(){
+		return nombre;
+	}
+	public void setNombre(String nombre){
+		nombre=nombre_;
+	}
+	
+	public String toString(){
+		return "nombre: "+nombre+" "+apellido1+" "+apellido2";
+	} 
+}
